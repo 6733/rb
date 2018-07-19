ba20ec3f5166dd3796feb60e660c8d8dfee578a9
MJ <mj@local>
2018-07-19 11:08:13 +0530

    added wizard

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -20,3 +20,210 @@ if ($('.clean-product').length > 0) {
     });
 }
 
+
+current_color = $('.wizard-card').data('color');
+full_color = true;
+
+$(document).ready(function(){
+
+     $('.fixed-plugin a, .fixed-plugin .badge').click(function(event){
+      // Alex if we click on switch, stop propagation of the event, so the dropdown will not be hide, otherwise we set the  section active
+        if($(this).hasClass('switch-trigger')){
+            if(event.stopPropagation){
+                event.stopPropagation();
+            }
+            else if(window.event){
+               window.event.cancelBubble = true;
+            }
+        }
+    });
+
+    $('.fixed-plugin .badge').click(function(){
+
+        $wizard = $('.wizard-card');
+        $button_next = $('.wizard-card .wizard-footer').find('.pull-right :input');
+
+
+
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+
+        var new_color = $(this).data('color');
+
+        $wizard.fadeOut('fast', function(){
+
+            $wizard.attr("data-color", new_color);
+            $button_next.removeClass(converterColor(current_color)).addClass(converterColor(new_color));
+
+            current_color = new_color;
+            $wizard.fadeIn('fast');
+        });
+    });
+
+    function converterColor(color){
+        switch (color) {
+            case 'blue':
+                return 'btn-info';
+                break;
+            case 'orange':
+                return 'btn-warning';
+                break;
+            case 'azure':
+                return 'btn-primary';
+                break;
+            case 'red':
+                return 'btn-danger';
+                break;
+            case 'green':
+                return 'btn-success';
+                break;
+        }
+    }
+});
+
+
+
+searchVisible = 0;
+transparent = true;
+
+        $(document).ready(function(){
+
+            /*  Activate the tooltips      */
+            $('[rel="tooltip"]').tooltip();
+
+            // Code for the Validator
+            /* var $validator = $('.wizard-card form').validate({
+        		  rules: {
+        		    firstname: {
+        		      required: true,
+        		      minlength: 3
+        		    },
+        		    lastname: {
+        		      required: true,
+        		      minlength: 3
+        		    },
+        		    email: {
+        		      required: true
+        		    }
+                },
+        	}); */
+
+            // Wizard Initialization
+          	$('.wizard-card').bootstrapWizard({
+                'tabClass': 'nav nav-pills',
+                'nextSelector': '.btn-next',
+                'previousSelector': '.btn-next',
+
+                onNext: function(tab, navigation, index) {
+                	/*var $valid = $('.wizard-card form').valid();
+                	if(!$valid) {
+                		$validator.focusInvalid();
+                		return false;
+                	} */
+                },
+
+                onInit : function(tab, navigation, index){
+
+                  //check number of tabs and fill the entire row
+                  var $total = navigation.find('li').length;
+                  $width = 100/$total;
+
+                  navigation.find('li').css('width',$width + '%');
+
+                },
+
+                onTabClick : function(tab, navigation, index){
+
+                   /* var $valid = $('.wizard-card form').valid();
+
+                    if(!$valid){
+                        return false;
+                    } else{
+                        return true;
+                    } */
+return true;
+                },
+
+                onTabShow: function(tab, navigation, index) {
+                    var $total = navigation.find('li').length;
+                    var $current = index+1;
+
+                    var $wizard = navigation.closest('.wizard-card');
+
+                    // If it's the last tab then hide the last button and show the finish instead
+                    if($current >= $total) {
+                        $($wizard).find('.btn-next').hide();
+                        $($wizard).find('.btn-finish').show();
+                    } else {
+                        $($wizard).find('.btn-next').show();
+                        $($wizard).find('.btn-finish').hide();
+                    }
+
+                    //update progress
+                    var move_distance = 100 / $total;
+                    move_distance = move_distance * (index) + move_distance / 2;
+
+                    $wizard.find($('.progress-bar')).css({width: move_distance + '%'});
+                    //e.relatedTarget // previous tab
+
+                    $wizard.find($('.wizard-card .nav-pills li.active a .icon-circle')).addClass('checked');
+
+                }
+	        });
+
+
+                // Prepare the preview for profile picture
+                $("#wizard-picture").change(function(){
+                    readURL(this);
+                });
+
+                $('[data-toggle="wizard-radio"]').click(function(){
+                    wizard = $(this).closest('.wizard-card');
+                    wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
+                    $(this).addClass('active');
+                    $(wizard).find('[type="radio"]').removeAttr('checked');
+                    $(this).find('[type="radio"]').attr('checked','true');
+                });
+
+                $('[data-toggle="wizard-checkbox"]').click(function(){
+                    if( $(this).hasClass('active')){
+                        $(this).removeClass('active');
+                        $(this).find('[type="checkbox"]').removeAttr('checked');
+                    } else {
+                        $(this).addClass('active');
+                        $(this).find('[type="checkbox"]').attr('checked','true');
+                    }
+                });
+
+                $('.set-full-height').css('height', 'auto');
+
+            });
+
+
+
+         //Function to show image before upload
+
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+                }
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+
+
+        function debounce(func, wait, immediate) {
+        	var timeout;
+        	return function() {
+        		var context = this, args = arguments;
+        		clearTimeout(timeout);
+        		timeout = setTimeout(function() {
+        			timeout = null;
+        			if (!immediate) func.apply(context, args);
+        		}, wait);
+        		if (immediate && !timeout) func.apply(context, args);
+        	};
+        };
